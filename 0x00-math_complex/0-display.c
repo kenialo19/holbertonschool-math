fd15fbbd3ae9714c055644db83eb72693a26e84d
kenialo19 <3396@holbertonschool.com>
2021-07-30 18:15:48 -0500

    modified file

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -9,4 +9,21 @@
  */
 void display_complex_number(complex c)
 {
+	if (c.im == 0 && c.re == 0)
+        {
+        }
+
+        else if (c.im == 0 && c.re != 0)
+        {
+                printf("%.0f\n", c.re);
+        }
+        else if (c.im != 0 && c.re == 0)
+        {
+                printf("%.0fi\n", c.im);
+        }
+        else
+        {
+                printf("%.0f + %.0fi\n", c.re, c.im);
+        }
+
 }
