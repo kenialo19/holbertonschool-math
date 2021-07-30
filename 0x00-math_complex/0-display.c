87509767738329ee650100b951e57502ffefc8c3
kenialo19 <3396@holbertonschool.com>
2021-07-30 16:51:46 -0500

    display numbers im, re

---
+++ 0x00-math_complex/0-display.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * display_complex_number - displays the complex numbers.
+ * @c: integer.
+ * 
+ * Return: void.
+ */
+void display_complex_number(complex c)
+{
+    if (c.im == 0 && c.re == 0)
+    {
+    }
+
+    else if (c.im == 0 && c.re != 0)
+    {
+        printf("%.0f\n", c.re);
+    }
+    else if (c.im != 0 && c.re == 0)
+    {
+        printf("%.0fi\n", c.im);
+    }
+    else
+    {
+        printf("%.0f + %.0fi\n",c.re, c.im);
+    }
+    
+}
---
+++ 0x00-math_complex/holberton.h
@@ -0,0 +1,21 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * struct complex - structure containing two doubles
+ * @re: part real.
+ * @im: part imaginary.
+ */
+
+typedef struct complex_t
+{
+    double re;
+    double im;
+
+} complex;
+
+/* PROTOTYPE*/
+
+void display_complex_number(complex c);
+
+#endif
