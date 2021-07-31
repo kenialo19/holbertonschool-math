69faed6d49f746ebe51b64b0d05d5f67fd272a98
kenialo19 <3396@holbertonschool.com>
2021-07-30 23:03:49 -0500

    modified file

--- 0x00-math_complex/2-modulus.c
+++ 0x00-math_complex/2-modulus.c
@@ -1,19 +1,19 @@
 #include <math.h>
 #include "holberton.h"
 
-
 /**
  * modulus - returns the modulus of a given complex number.
  * @c: complex number.
- * 
+ *
  * Return: modulus
  */
 
 double modulus(complex c)
 {
-    double x = 0, y = 0;
-    x = ((c.re * c.re) + (c.im * c.im));
-    y = sqrt(x);
+	double x = 0, y = 0;
 
-    return (y);
+	x = ((c.re * c.re) + (c.im * c.im));
+	y = sqrt(x);
+
+	return (y);
 }
